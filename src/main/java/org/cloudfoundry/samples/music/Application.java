2ee87238aa065580539905718fb83dd590a869a7
ShaliniGB <shalugb89@gmail.com>
2018-09-14 17:29:36 +0530

    Demo with Spring Security basic details

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableResourceServer
+
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
