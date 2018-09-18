7fa05f9dadd7a5d96a2fb15732c7508e0b8c4f89
ShaliniGB <shalugb89@gmail.com>
2018-09-18 18:01:49 +0530

    Demo with slueth

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import java.util.logging.Logger;
 
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-
+    private static final Logger LOG = Logger.getLogger(Application.class.getName());
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).
                 initializers(new SpringApplicationContextInitializer())
